06575bda73da7fdd9de346f025cf31d8d4b5e02b
cmtat <carter.m.tatum@gmail.com>
2025-10-01 20:11:22 +0000

    -

--- test/firebaseConfig.sample.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: 'YOUR_API_KEY',
-  authDomain: 'your-project-id.firebaseapp.com',
-  projectId: 'your-project-id',
-  appId: 'YOUR_APP_ID',
-};
-
-// Copy this file to firebaseConfig.js and replace values with your Firebase project settings.
